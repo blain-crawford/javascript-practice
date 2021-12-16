d6749cf92425458a8a86bb97ec36a6e0ca3200f2
Blain Crawford <blaincrawford@gmail.com>
2021-12-16 16:28:23 -0500

    working with math equations

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,34 @@
+// 1 Add 2 numbers together! (just type console.log(23 + 97) into your html file)
+// 2 Add a sequence of 6 different numbers together.
+// 3 Print the solution to the following equation: (4 + 6 + 9) / 77
+// 4 Answer should be approximately 0.24675
+// 5 Let’s use variables!
+// 6 Type the following at the top of the script tag: let a = 10
+// 7 console.log(a) should print 10
+// 8 Try the following: 9 * a
+// 9 and this: let b = 7 * a (returns undefined *) and then console.log(b)
+// 10 You should be getting the hang of this by now… try this sequence:
+// 11 Declare a constant variable max with the value 57
+// 12 Set another variable actual to max - 13
+// 13 Set another variable percentage to actual / max
+// 14 If you type percentage in the console and press enter you should see a value like 0.7719
+// 15 Take a few minutes to keep playing around with various things in your script tag. Eventually, we will learn how to actually make those numbers and things show up on the webpage, but all of this logic will remain the same, so make sure you’re comfortable with it before moving on.
+// 16 * As you might have noticed by running Javascript code in the console, the console prints the result of the code it executes (called a return statement). You will learn more about these in the next lessons, however for now it is good to remember that an assignment (such as b = 7 * a) returns undefined and so you cannot assign a value to a variable and read its value in the same line.`
+
+
+
 let myDiv = document.getElementById('test');
 let myHeader = document.createElement('h1');
-myHeader.textContent = 'Hello, World';
+let myMod = 475 /Math.floor((67 % 17))
+
+// myHeader.textContent = Math.floor(myMod);
+
+// myDiv.appendChild(myHeader);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+
+myHeader.textContent = percentage;
 
 myDiv.appendChild(myHeader);
