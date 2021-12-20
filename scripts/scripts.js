2a18365934a718953260abdbb9a4bebdd7849a35
Blain Crawford <blaincrawford@gmail.com>
2021-12-20 11:36:54 -0500

    adding in more constructors for linked list

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -56,26 +56,115 @@ let myMod = 475 / Math.floor(67 % 17);
 // askAge();
 
 
-const n1 = {
-  data: 100
-}
+// const n1 = {
+//   data: 100
+// }
 
-const n2 = {
-  data: 200
-}
+// const n2 = {
+//   data: 200
+// }
 
-n1.next = n2;
+// n1.next = n2;
 
 
+
+// class Node {
+//   constructor(data, next = null) {
+//     this.data = data;
+//     this.next = next;
+//   }
+// }
+
+// const n3 = new Node(300);
+// n1.next = n3
+
+// console.log(n1);
 
 class Node {
   constructor(data, next = null) {
     this.data = data;
     this.next = next;
   }
-}
+}; 
 
-const n3 = new Node(300);
-n1.next = n3
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
 
-console.log(n1);
+  // Insert First Node
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+    this.size++;
+  }
+  // Insert Last node
+  insertLast(data){
+    let node = new Node(data);
+    let current;
+    if(!this.head){
+      this.head = node;
+    } else {
+        current = this.head;
+        while(current.next) {
+          current = current.next;
+        }
+        current.next = node;
+    }
+    this.size++;
+  }
+
+  // Insert at index
+  insertAtIndex(data, index){
+    if(index > 0 && index > this.size){
+      return;
+    }
+
+    if(index === 0){
+      this.head = new Node(data, this.head);
+      return;
+    }
+
+    const node = new Node(data);
+    let current, previous;
+    current = this.head;
+    let count = 0;
+
+    while(count < index){
+      previous = current;
+      count++;
+      current = current.next;
+    }
+    node.next = current;
+    previous.next = node;
+    this.size++
+  }
+
+  // Get at index
+
+  // Remove at index
+
+  // Clear list
+
+  // Print list data
+  printListData(){
+    let current = this.head;
+
+    while(current){
+      console.log(current.data);
+      current = current.next
+    }
+  }
+};
+
+const ll = new LinkedList();
+ll.insertFirst(100);
+ll.insertFirst(50);
+ll.insertFirst(25)
+ll.insertLast(125);
+ll.insertLast(150)
+ll.insertAtIndex(130, 3);
+ll.insertAtIndex(20, 0);
+ll.insertAtIndex(765, 19);
+ll.printListData();
+
