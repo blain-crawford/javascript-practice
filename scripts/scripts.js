374b7eda81218b5a8c1b422c936ecd8837275eed
Blain Crawford <blaincrawford@gmail.com>
2021-12-20 14:37:00 -0500

    some more basi cmath warmups

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -201,21 +201,23 @@ let myMod = 475 / Math.floor(67 % 17);
 
 
 
-function troubleshooting() {
-	const a = 1;
-	const b = 1;
+/* Some rules first:
+*   Enter the operations, replacing the `"?"`, make the computer do the work for you. 
+*   Do not manually enter the answer, for example: "one plus ten" would look like 1+10
+* 
+* a = one plus eight
+* b = 22 times three
+* c = the *remainder* of 5/4
+* d = the variable 'a' minus 17
+* e = the sum of the previous four variables
+*/
 
-	let result;
+const a = 1 + 8;
+const b = 22 * 3;
+const c = Math.floor(5 / 4);
+const d = a - 17;
+const e = a + b + c + d;
 
-	// Edit between these lines
-	// =================================
-	result = a + b;
-
-	// =================================
-
-	return result;
-}
-
-let myResult = troubleshooting();
-
-console.log(myResult);
+// module.exports = {a, b, c, d, e}
+ 
+console.log(a, b, c ,d ,e);
